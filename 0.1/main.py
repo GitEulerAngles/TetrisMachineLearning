e3c3e04dd120a984677460ba5b4c65781ec00e96
GitEulerAngles <62534632+GitEulerAngles@users.noreply.github.com>
2024-10-01 15:45:50 -0500

    Update main.py

--- 0.1/main.py
+++ 0.1/main.py
@@ -1,43 +1,99 @@
+from pyboy import pyboy
+from machine import machine
+from gameboy import game
+from time import time
 import tensorflow as tf
-import numpy as np
+print(tf.__version__)
 
-class machine:
-    model = 0
-    def __init__(self):
-        self.model = tf.keras.Sequential([
-            tf.keras.layers.Dense(10, activation='linear', input_shape=(180,)),
-            tf.keras.layers.Dense(20, activation='linear'),
-            tf.keras.layers.Dense(10, activation='linear'),
-            tf.keras.layers.Dense(5, activation='linear')
-        ])
+amount_of_games = 1
+games = []
+models = []
+game_over = []
 
-        self.model.compile(optimizer='adam', loss='binary_crossentropy')
-    
-    def randomFit(self, scale=.2, max_value=10.0):
-        for layer in self.model.layers:
-            current_weights = layer.get_weights()
-            new_weights = []
-            for w in current_weights:  # This will loop over weights and biases together
-                r = (np.random.random(w.shape) * scale * 2) - scale
-                # Apply the randomness
-                modified_w = np.clip(w + r, -max_value, max_value)
-                new_weights.append(modified_w)
-            layer.set_weights(new_weights)
+for i in range(amount_of_games):
+    game_over.append(0)
+    newModel = machine()
+    if amount_of_games == 0:
+        newModel.model = newModel.model.load_weights("C:/PythonSaves/Emulator/model.h5")
+    models.append(newModel)
+    newGame = game()
+    newGame.emulation.tick()
+    for i in range(170):
+        newGame.emulation.send_input(pyboy.WindowEvent.PRESS_BUTTON_START)
+        newGame.emulation.tick()
+        newGame.emulation.send_input(pyboy.WindowEvent.RELEASE_BUTTON_START)
+        newGame.emulation.tick()
+    games.append(newGame)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
 
-    def runModel(self, game_tiles):
-        inputData = []
-        for y in range(0,18):
-            for x in range(0,10):
-                inputData.append(game_tiles[y][x])
-
-        inputData = tf.convert_to_tensor(inputData, dtype=tf.float32)
-        inputData = tf.reshape(inputData, (1, 180))
-        
-        predictions = self.model.predict(inputData)[0]
-
+reset = 0
+clock = 0
+while 1:
+    clock += 1
+    for i in range(amount_of_games):
+        if games[i].wrap.game_over():
+            if game_over[i] == 0:
+                game_over[i] = 1
+                reset += 1
+        games[i].updateData()
+        button = -1
+        if clock >= 80:
+            button = models[i].runModel(games[i].game_tiles)
+            if i == amount_of_games-1:
+                clock = 0
+        if (game_over[i] == 0):
+            games[i].runGame(button)
+    if (reset == amount_of_games):
         maxIndex = 0
-        for i in range(1,5):
-            if predictions[i] > predictions[maxIndex]:
+        reset = 0
+        for i in range(amount_of_games):
+            game_over[i] = 0
+            lineCoverage = 0
+
+            for y in range(0,18):
+                count = 0
+                for x in range(0,10):
+                    if (games[i].game_tiles[y][x] == 1):
+                        count += 1
+                lineCoverage += count^2
+
+            for x in range(0,10):
+                penalty = False
+                for y in range(17,-1,-1):
+                    if (games[i].game_tiles[y][x] == 1):
+                        penalty = True
+                    if (games[i].game_tiles[y][x] == 0 and penalty):
+                        games[i].fitNess -= 5
+
+            games[i].fitNess += games[i].wrap.score*20
+            games[i].fitNess += lineCoverage
+
+            if games[i].fitNess > games[maxIndex].fitNess:
                 maxIndex = i
         
-        return maxIndex
+        print("Max index for that generation: " + str(games[maxIndex].fitNess))
+
+        models[maxIndex].model.save_weights("model.h5")
+        for i in range(amount_of_games):
+            games[i].fitNess = 0
+
+            if i == maxIndex:
+                continue
+            models[i].model.set_weights(models[maxIndex].model.get_weights())
+            models[i].randomFit(.1,10.0)
+        for i in range(amount_of_games):
+            while (games[i].tile[0,0] == 298):
+                games[i].emulation.send_input(pyboy.WindowEvent.PRESS_BUTTON_START)
+                games[i].emulation.tick()
+                games[i].emulation.send_input(pyboy.WindowEvent.RELEASE_BUTTON_START)
+                games[i].emulation.tick()
